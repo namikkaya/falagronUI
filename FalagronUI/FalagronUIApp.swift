5de3b2ff4a10c9c993385e5e0dc3d87e48b04190
namikkaya <namikkaya86@gmail.com>
2022-09-03 17:32:39 +0300

    Initial Commit

---
+++ FalagronUI/FalagronUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FalagronUIApp.swift
+//  FalagronUI
+//
+//  Created by namik kaya on 3.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct FalagronUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
